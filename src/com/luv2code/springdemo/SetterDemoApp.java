7faffc4bef2c96d822a4c48baeca3648d81ee8e7
Ziqi Chen <ziqichen@umass.edu>
2019-11-07 23:35:14 -0500

    updates

--- src/com/luv2code/springdemo/SetterDemoApp.java
+++ src/com/luv2code/springdemo/SetterDemoApp.java
@@ -17,6 +17,12 @@ public class SetterDemoApp {
 
         System.out.println(theCoach.getDailyFortune());
 
+
+        // call our new methods to get the literal values
+        System.out.println(theCoach.getEmailAddress());
+
+        System.out.println(theCoach.getTeam());
+
         // close the context
         context.close();
     }
