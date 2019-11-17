ab4bb768c2d73b72274bd43ebcd4867197c578ec
Ziqi Chen <ziqichen@umass.edu>
2019-11-16 23:14:17 -0500

    Bean Scope & LifeCycle

--- src/com/luv2code/springdemo/TrackCoach.java
+++ src/com/luv2code/springdemo/TrackCoach.java
@@ -29,4 +29,15 @@ public class TrackCoach implements Coach {
     }
 
 
+    // add an init method
+    public void doMyStartupStuff() {
+        System.out.println("TrackCoach: inside method doMyStartupStuff");
+    }
+
+
+    // add a destroy method
+    public void doMyCleanupStuffYoYo() {
+        System.out.println("TrackCoach: inside method doMyCleanupStuffYoYo");
+    }
+
 }
